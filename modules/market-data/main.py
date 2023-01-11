6abbd2f5c340ae6fe71ca10d5b122696c9f8b0de
ZenithClown <debmalyapramanik.005@gmail.com>
2023-01-11 13:59:54 +0530

    🚧 create logic to automatically rename file based on report date

--- modules/market-data/main.py
+++ modules/market-data/main.py
@@ -12,12 +12,15 @@ in a local file system.
 """
 
 import os # miscellaneous os interfaces
+import sys # object manipulation of system
+import json # read and manipulate json objects
 import time # miscellaneous for time manipulation
+import datetime as dt # module to manipulate datetime
 
 from utils_ import * # noqa: F403 # pylint: disable:unused-import
 from webdriver import create_chromedriver
 
-def download(report : str, driver : object, outpath : str, outfile : str = None) -> bool:
+def download(report : str, driver : object, outpath : str, outfile : tuple = tuple()) -> bool:
     URL = "https://www.iexindia.com/marketdata/{extension}.aspx"
 
     # set the driver with additional parameters
@@ -42,11 +45,16 @@ def download(report : str, driver : object, outpath : str, outfile : str = None)
     timer_(11, desc = "Downloading Excel File")
     driver.quit() # safely close the driver, release cache
 
+    # once the file is downloaded, rename the file using `os.rename`
+    os.rename(os.path.join(outpath, outfile[0]), os.path.join(outpath, outfile[1]))
     return True
 
 
 if __name__ == "__main__":
     print(f"{time.ctime()} | Starting IEX Market Data Downloads...")
+
+    # get the additional arguments using `sys.argv`
+    _, date = sys.argv
 
     # get the driver with configurations from `../utilities/`
     _, driver = create_chromedriver()
@@ -57,6 +65,12 @@ if __name__ == "__main__":
         "market_snapshot", # marketdata/dam/marketsnapshot
     ]
 
+    # read general configurations of each report from `config.json`
+    config = json.load(open("config.json", "r"))
+
+    # download report for the date
+    date = dt.datetime.strptime(date, "%Y-%m-%d")
+
     for report in REPORTS:
         # each report can be downloaded invoking `download()`, all
         # files are downloaded into a specific database folder in
@@ -65,5 +79,9 @@ if __name__ == "__main__":
         # controlled from this file
         download(
             report = report, driver = driver,
-            outpath = os.path.join(os.getcwd(), "data")
+            outpath = os.path.join(os.getcwd(), "data"),
+            outfile = (
+                config[report]["default_name"],
+                config[report]["final_name"].format(date = f"{date.year}-{str(date.month).zfill(2)}-{str(date.day).zfill(2)}")
+            ),
         )
